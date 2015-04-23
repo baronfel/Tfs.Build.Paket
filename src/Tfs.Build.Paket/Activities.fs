45536ca20150f3e54ffc4f7499f941af9963f3a4
Chet Husk <chet_husk@volusion.com>
2015-04-23 11:48:57 -0500

    pull the types out of the module in case the static class that's compiled isn't usable by the tfs workflows

--- src/Tfs.Build.Paket/Activities.fs
+++ src/Tfs.Build.Paket/Activities.fs
@@ -1,5 +1,7 @@
 ﻿namespace Tfs.Build.Paket
 
+open Tfs.Build.Paket.PaketHelpers
+
 type In<'a> = System.Activities.InArgument<'a>
 type Out<'a> = System.Activities.OutArgument<'a>
 
@@ -10,10 +12,6 @@ module Activities =
     open Tfs.Build.Paket.PaketHelpers
     open Microsoft.TeamFoundation.Build.Workflow.Activities
 
-    type PaketCallStatus = 
-        | Successful = 0
-        | Failed = 1
-
     let logMsg (context : System.Activities.CodeActivityContext) msg = context.TrackBuildMessage(msg, Microsoft.TeamFoundation.Build.Client.BuildMessageImportance.Normal)
     let logErr (context : System.Activities.CodeActivityContext) msg = context.TrackBuildError(msg)
     let setResult (context : System.Activities.CodeActivityContext) (outProp : Out<'a>) trueVal falseVal input =
@@ -21,44 +19,48 @@ module Activities =
          | true -> context.SetValue(outProp, trueVal)
          | false -> context.SetValue(outProp, falseVal) 
 
-    type RestoreActivity() =
-        inherit System.Activities.CodeActivity()
+type PaketCallStatus = 
+        | Successful = 0
+        | Failed = 1
 
-        member val SourceFolder : In<string> = null with get,set
-        member val Status : Out<int> = null with get,set
+type RestoreActivity() =
+    inherit System.Activities.CodeActivity()
 
-        override x.Execute context =
-            let sourceFolder = context.GetValue x.SourceFolder 
+    member val SourceFolder : In<string> = null with get,set
+    member val Status : Out<int> = null with get,set
 
-            restoreFromSourceDir sourceFolder (logErr context) (logMsg context)
-            |> setResult context x.Status (int PaketCallStatus.Successful) (int PaketCallStatus.Failed)
+    override x.Execute context =
+        let sourceFolder = context.GetValue x.SourceFolder 
 
-    type AssertNoPrereleaseActivity() =
-        inherit System.Activities.CodeActivity()
+        restoreFromSourceDir sourceFolder (Activities.logErr context) (Activities.logMsg context)
+        |> Activities.setResult context x.Status (int PaketCallStatus.Successful) (int PaketCallStatus.Failed)
 
-        member val SourceFolder : In<string> = null with get,set
-        member val Status : Out<int> = null with get,set
+type AssertNoPrereleaseActivity() =
+    inherit System.Activities.CodeActivity()
 
-        override x.Execute context =
-            let sourceFolder = context.GetValue x.SourceFolder
-            hasPrereleases sourceFolder (logErr context) (logMsg context)
-            |> setResult context x.Status (int PaketCallStatus.Successful) (int PaketCallStatus.Failed)
+    member val SourceFolder : In<string> = null with get,set
+    member val Status : Out<int> = null with get,set
+
+    override x.Execute context =
+        let sourceFolder = context.GetValue x.SourceFolder
+        hasPrereleases sourceFolder (Activities.logErr context) (Activities.logMsg context)
+        |> Activities.setResult context x.Status (int PaketCallStatus.Successful) (int PaketCallStatus.Failed)
     
-    type AssertNoUnapprovedFeedsActivity() =
-        inherit System.Activities.CodeActivity()
+type AssertNoUnapprovedFeedsActivity() =
+    inherit System.Activities.CodeActivity()
 
-        member val SourceFolder : In<string> = null with get,set
-        member val Status : Out<int> = null with get,set
+    member val SourceFolder : In<string> = null with get,set
+    member val Status : Out<int> = null with get,set
 
-        member val AllowedFeeds : In<ResizeArray<string>> = null with get,set
-        member val ShouldError : In<bool> = null with get,set
+    member val AllowedFeeds : In<ResizeArray<string>> = null with get,set
+    member val ShouldError : In<bool> = null with get,set
 
-        override x.Execute context = 
-            let sourceFolder = context.GetValue x.SourceFolder
-            let feeds = context.GetValue x.AllowedFeeds |> List.ofSeq
-            let failOnInvalids = context.GetValue x.ShouldError
+    override x.Execute context = 
+        let sourceFolder = context.GetValue x.SourceFolder
+        let feeds = context.GetValue x.AllowedFeeds |> List.ofSeq
+        let failOnInvalids = context.GetValue x.ShouldError
 
-            hasInvalidSources sourceFolder feeds failOnInvalids (logErr context) (logMsg context)
-            |> not
-            |> setResult context x.Status (int PaketCallStatus.Successful) (int PaketCallStatus.Failed)
+        hasInvalidSources sourceFolder feeds failOnInvalids (Activities.logErr context) (Activities.logMsg context)
+        |> not
+        |> Activities.setResult context x.Status (int PaketCallStatus.Successful) (int PaketCallStatus.Failed)
 
